d1bb2e7a910f8eac1260b46354a6f3a6070c8892
mxciv <298858723+mxciv@users.noreply.github.com>
2026-07-09 14:07:35 +0700

    Update aha.lua

--- aha.lua
+++ aha.lua
@@ -1,71 +1,67 @@
 
--- Configuration
-local Config = {
-    PATIENT_ATTACH_PART = "RootPart", -- or "Head"
-    PATIENT_DEFAULT_COLOR = Color3.fromHex("#FFFFFF"),
-    IGNORE_PATIENTS = {"Doctor"}
-}
-
--- Services
 local Workspace = game:GetService("Workspace")
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
 
-local LocalPlayer = Players.LocalPlayer
-local playerCharacter = LocalPlayer.Character
+local Character = Players.LocalPlayer.Character
+local NPC = Workspace.NPCs
+local Rooms = Workspace.Rooms
 
--- Variables
-local hospitalRooms = {}
-local patientActiveESP = {}
-local medicineCache = {}
+local activePatient = {}
+local npcRooms = {}
 
--- Utilities
-local function checkIgnoredPatient(name)
-    for _, ignoreName in pairs(Config.IGNORE_PATIENTS) do
-        if string.match(name, ignoreName) then
-            return true
-        end
-    end
-    return false
+if not NPC then
+    return error("Can't find the NPC folder!")
+end
+
+if not Rooms then
+    return error("Can't find the Rooms folder!")
 end
 
 local function getDistanceFromPlayer(position, studs_per_unit)
-	if not position then return 0 end
-    if not playerCharacter then return 0 end
+    if not position then return 0 end
 
-    local hrp = playerCharacter:FindFirstChild("HumanoidRootPart")
+    local hrp = Character:FindFirstChild("HumanoidRootPart")
     if not hrp then return 0 end
 
-    return (hrp.Position - position).Magnitude / studs_per_unit
+    return math.floor((hrp.Position - position).Magnitude / studs_per_unit)
 end
 
-local function getPatientColor(patient)
-    if not patient then 
-        warn("Patient instance is not valid!") 
-        return Config.PATIENT_DEFAULT_COLOR 
-    end
-
-    if patient:GetAttribute("Skinwalker") or patient:GetAttribute("IsAnomaly") then
-        return Color3.fromHex("#FF0000")
-    end
-    if string.match(patient.Name, "onster") then
-        return Color3.fromHex("#FFFF00")
-    end
-    if patient:GetAttribute("IsVisitor") then
-        return Color3.fromHex("#00FF00")
-    end
-    if patient:GetAttribute("IsPatient") then
-        return Color3.fromHex("#55AAFF")
-    end
+local function getMedicineInfo(roomName)
+    local roomInstance = npcRooms[roomName]
+    if not roomInstance then return "" end
     
-    return Config.PATIENT_DEFAULT_COLOR
+    -- Peak child
+    local reports = roomInstance:FindFirstChild("Minigame") 
+        and roomInstance.Minigame:FindFirstChild("TV") 
+        and roomInstance.Minigame.TV:FindFirstChild("Screen") 
+        and roomInstance.Minigame.TV.Screen:FindFirstChild("UI") 
+        and roomInstance.Minigame.TV.Screen.UI:FindFirstChild("Report") 
+        and roomInstance.Minigame.TV.Screen.UI.Report:FindFirstChild("inv")
+    
+    if reports then
+        local names = {}
+        for _, med in pairs(reports:GetChildren()) do
+            if med.Name == "UIGridLayout" then continue end
+            table.insert(names, med.Name)
+        end
+
+        return table.concat(names, " | ")
+    end
+
+    return ""
 end
 
--- ESP Renderer
-local function processPatientESP(part, color, room)
-    if not part then return warn("Invalid part instance!") end
+local function processRoomMonster(monster)
+    if not monster then return end
+    
+    local main = monster:FindFirstChild("Main")
+    if not main then return end
 
-    local espDist = Drawing.new("Text")
+    if activePatient[main.Address] then return end
+
+    -- Render ESP
+   local espDist = Drawing.new("Text")
     espDist.Visible = true
     espDist.Transparency = 1
     espDist.ZIndex = 20
@@ -81,9 +77,77 @@ local function processPatientESP(part, color, room)
     espName.Visible = true
     espName.Transparency = 1
     espName.ZIndex = 10
-    espName.Color = color
+    espName.Color = Color3.fromHex("#FF0000")
     espName.Position = Vector2.new(0, 25)
-    espName.Text = part.Parent.Name
+    espName.Text = monster.Name or "Unknown"
+    espName.Size = 20
+    espName.Center = true
+    espName.Outline = true
+    espName.Font = Drawing.Fonts.Monospace
+
+    -- Store everything in this
+    activePatient[main.Address] = {
+        Part = main,
+        Instances = {
+            Distance = espDist,
+            Name = espName,
+            Meds = nil
+        }
+    }
+end
+
+local function processPatient(patient)
+    if not patient then return end
+
+    -- Atributes data
+    local isAnomaly = (patient:GetAttribute("Skinwalker") or patient:GetAttribute("IsAnomaly")) or false
+    local isVisitor = patient:GetAttribute("IsVisitor") or false
+    local isPatient = patient:GetAttribute("IsPatient") or false
+    local isTreated = patient:GetAttribute("Treated") or false
+    local patientRooms = patient:GetAttribute("DesignatedRoom") or ""
+
+    -- Get Color
+    local patientColor = Color3.fromHex("#FFFFFF")
+    if      isAnomaly then patientColor = Color3.fromHex("#FF0000")
+    elseif  isVisitor then patientColor = Color3.fromHex("#00FF00")
+    elseif  isPatient then patientColor = Color3.fromHex("#55AAFF") end
+
+    -- Check for hrp
+    local hrp = patient:FindFirstChild("HumanoidRootPart")
+    if not hrp then return end
+
+    -- Update active ESP data
+    if activePatient[hrp.Address] then
+        local data = activePatient[hrp.Address]
+        data.Instances.Name.Text = patient.Name
+        data.Instances.Name.Color = patientColor
+
+        if not isVisitor then 
+            data.Instances.Meds.Text = (not isTreated) and getMedicineInfo(patientRooms) or ""
+        end
+        return
+    end
+
+    -- Render ESP
+   local espDist = Drawing.new("Text")
+    espDist.Visible = true
+    espDist.Transparency = 1
+    espDist.ZIndex = 20
+    espDist.Color = Color3.fromHex("#FFFFFF")
+    espDist.Position = Vector2.new(0, 0)
+    espDist.Text = "[24 m]"
+    espDist.Size = 18
+    espDist.Center = true
+    espDist.Outline = true
+    espDist.Font = Drawing.Fonts.Monospace
+
+    local espName = Drawing.new("Text")
+    espName.Visible = true
+    espName.Transparency = 1
+    espName.ZIndex = 10
+    espName.Color = patientColor
+    espName.Position = Vector2.new(0, 25)
+    espName.Text = patient.Name or "Unknown Patient"
     espName.Size = 20
     espName.Center = true
     espName.Outline = true
@@ -101,165 +165,108 @@ local function processPatientESP(part, color, room)
     espMeds.Outline = true
     espMeds.Font = Drawing.Fonts.Monospace
 
-    patientActiveESP[part.Address] = {
-        Instance = part,
-        EspDist = espDist,
-        EspName = espName,
-        EspMeds = espMeds,
-        RoomName = room,
+    -- Store everything in this
+    activePatient[hrp.Address] = {
+        Part = hrp,
+        Instances = {
+            Distance = espDist,
+            Name = espName,
+            Meds = espMeds
+        }
     }
 end
 
-local function deletePatientESP(address)
-    local data = patientActiveESP[address]
+local function disconnectESP(address)
+    local data = activePatient[address]
+    if not data then return end
 
-    if data then
-        if data.EspDist then data.EspDist:Remove() end
-        if data.EspName then data.EspName:Remove() end
-        if data.EspMeds then data.EspMeds:Remove() end
-        patientActiveESP[address] = nil
+    data.Instances.Distance:Remove()
+    data.Instances.Name:Remove()
+
+    if data.Instances.Meds then 
+        data.Instances.Meds:Remove() 
+    end
+
+    activePatient[address] = nil
+end
+
+-- Initialize rooms instance
+for _, room in pairs(Rooms:GetDescendants()) do
+    if string.match(room.Name, "^Room%d$") then
+        npcRooms[room.Name] = room
     end
 end
 
-local function updatePatientESP()
-    for address, data in pairs(patientActiveESP) do
-        local part = data.Instance
+task.spawn(function()
+    while true do
+        for _, kid in pairs(NPC:GetChildren()) do
+			if kid.Name == "Doctor" then continue end
+            processPatient(kid)
+        end
+
+        for _, room in pairs(npcRooms) do
+            local monster = room:FindFirstChild("EyeMass") or room:FindFirstChild("MonsterBed")
+
+            if monster then
+                processRoomMonster(monster)
+            end
+        end
+
+        task.wait(1) -- scan every second
+    end
+end)
+
+RunService.RenderStepped:Connect(function()
+    for address, data in pairs(activePatient) do
+        local part = data.Part
 
         if not part then
-            deletePatientESP(address)
+            disconnectESP(address)
             continue
         end
 
         if not part:IsDescendantOf(Workspace) then
-            deletePatientESP(address)
+            disconnectESP(address)
             continue
         end
 
         local partPosition = part.Position
 
         if not partPosition then
-            deletePatientESP(address)
+            disconnectESP(address)
             continue
         end
 
-        local screnPosition, onScreen = WorldToScreen(partPosition)
-
-        if not onScreen then
-            data.EspDist.Visible = false
-            data.EspName.Visible = false
-            data.EspMeds.Visible = false
-            continue
-        end
+        local screenPosition, onScreen = WorldToScreen(partPosition)
         
-        -- Update drawings
-        local distance = getDistanceFromPlayer(part.Position, 3)
+        if not onScreen then
+            data.Instances.Distance.Visible = false
+            data.Instances.Name.Visible     = false
+            if data.Instances.Meds then data.Instances.Meds.Visible = false end
+            continue
+        end
 
-        data.EspDist.Text = string.format("[%d m]", distance)
-        data.EspDist.Position = screnPosition + Vector2.new(0, -10)
-        data.EspDist.Visible = true
+        local distance = getDistanceFromPlayer(partPosition, 3)
 
-        data.EspName.Position = screnPosition + Vector2.new(0, 10)
-        data.EspName.Visible = true
-
-        data.EspMeds.Position = screnPosition + Vector2.new(0, 35)
-        data.EspMeds.Visible = (data.EspMeds.Text ~= "")
-    end
-end
-
--- Main Process
-local function updateMedicineInfo()
-    for roomName, rooms in pairs(hospitalRooms) do
-        local medicines = rooms:FindFirstChild("Minigame") 
-                and rooms.Minigame:FindFirstChild("TV") 
-                and rooms.Minigame.TV:FindFirstChild("Screen") 
-                and rooms.Minigame.TV.Screen:FindFirstChild("UI") 
-                and rooms.Minigame.TV.Screen.UI:FindFirstChild("Report")
-                and rooms.Minigame.TV.Screen.UI.Report:FindFirstChild("inv")
-
-        if medicines then
-            local names = {}
-            for _, med in pairs(medicines:GetChildren()) do
-                if med.Name == "UIGridLayout" then continue end
-                table.insert(names, med.Name)
-            end
-
-            medicineCache[roomName] = table.concat(names, " | ")
-        else
-            medicineCache[roomName] = ""
+        data.Instances.Distance.Visible     = true
+        data.Instances.Distance.Text        = "["..distance.." m]"
+        data.Instances.Distance.Position    = Vector2.new(
+            screenPosition.X,
+            screenPosition.Y - 10
+        )
+        
+        data.Instances.Name.Visible         = true
+        data.Instances.Name.Position        = Vector2.new(
+            screenPosition.X,
+            screenPosition.Y + 10
+        )
+        
+        if data.Instances.Meds then
+            data.Instances.Meds.Visible     = true
+            data.Instances.Meds.Position    = Vector2.new(
+                screenPosition.X,
+                screenPosition.Y + 35
+            )
         end
     end
-
-    for _, data in pairs(patientActiveESP) do
-        if not data.Instance.Parent:GetAttribute("Treated") or not data.Instance.Parent:GetAttribute("IsVisitor") then
-
-            if data.RoomName then
-                data.EspMeds.Text = medicineCache[data.RoomName]
-            end
-        end
-    end
-end
-
-local function processPatient(patient)
-    local part = patient:FindFirstChild(Config.PATIENT_ATTACH_PART)
-    if not part then return end
-    
-    local data = patientActiveESP[part.Address]
-    local room = patient:GetAttribute("DesignatedRoom")
-    local color = getPatientColor(patient)
-
-    -- Sync data if active
-    if data then 
-        data.EspName.Text = patient.Name
-        data.EspName.Color = color
-        data.RoomName = room
-        return 
-    end
-
-    -- Stalker detector
-    if string.match(patient.Name, "TallMonster") then
-        notify("Stalker is spawned, avoid eye contact!", 8)
-    end
-
-    processPatientESP(part, getPatientColor(patient), room)
-end
-
-local function initializeScanner(location, wait_time)
-    if not location then return error("Invalid location instance!") end
-
-    -- run service for updatePatientESP
-    RunService.RenderStepped:Connect(updatePatientESP)
-
-    -- spawn new task for our npc scanner
-    task.spawn(function()
-        while true do
-            for _, kid in pairs(location:GetChildren()) do
-                if not checkIgnoredPatient(kid.Name) then
-                    processPatient(kid)
-                end
-            end
-            
-            -- Handle EyeMass
-            local EyeMass = Workspace.Rooms:FindFirstChild("EyeMass")
-
-            if EyeMass then
-                local part = EyeMass:FindFirstChild("Main")
-                if part and not patientActiveESP[part.Address] then
-                    processPatientESP(part, Color3.fromHex("#FF0000"), nil)
-                end
-            end
-
-            updateMedicineInfo()
-            task.wait(wait_time)
-        end
-    end)
-end
-
--- scan for room instances
-for _, room in pairs(Workspace.Rooms:GetDescendants()) do
-    if room:IsA("Folder") and string.match(room.Name, "^Room%d$") then
-        hospitalRooms[room.Name] = room
-    end
-end
-
--- Initializer
-initializeScanner(Workspace.NPCs, 1)
+end)
